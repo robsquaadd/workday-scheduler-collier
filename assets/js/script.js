07ee972c508bda7761c6f311b248e405327c60e0
robsquaadd <102495137+robsquaadd@users.noreply.github.com>
2022-05-21 22:26:40 -0400

    relative time features added, local storage error fixed.

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,7 +31,7 @@ const generateHours = () => {
     hourBlockEl.classList = "row";
     calendarEl.appendChild(hourBlockEl);
     var hourTextEl = document.createElement("p");
-    hourTextEl.classList = "col-2";
+    hourTextEl.classList = "col-2 hour";
     if (i + 9 < 12) {
       hourTextEl.textContent = `${i + 9} AM`;
     } else if (i + 9 == 12) {
@@ -40,7 +40,7 @@ const generateHours = () => {
       hourTextEl.textContent = `${i - 3} PM`;
     }
     var eventEl = document.createElement("input");
-    eventEl.classList = "col-8";
+    eventEl.classList = "col-8 eventInput";
     eventEl.setAttribute("id", `hourInput${i}`);
     var saveButtonEl = document.createElement("button");
     saveButtonEl.classList = "col-2 btn-primary";
@@ -54,9 +54,7 @@ const generateHours = () => {
       //grabbed the input element that corresponds to the save button that I clicked.
       var inputEl = document.getElementById(`hourInput${hourIndex}`);
       //stored the text content of the input element to the taskInfoArray.
-      console.log(inputEl);
       taskInfoArray[hourIndex] = inputEl.value;
-      console.log(taskInfoArray);
       //called the storeEvents function to store the events in local storage.
       storeEvents();
     });
@@ -78,10 +76,27 @@ const storeEvents = () => {
 // getItem retrieves the item from localStorage
 
 //use bootstrap to change the color of the hour elements if that time is in the past, present, or future
+const relativeTime = () => {
+  var currentHour = moment().hour();
+  var inputElArray = document.querySelectorAll(".eventInput");
+  for (i = 0; i < inputElArray.length; i++) {
+    var inputElementId = inputElArray[i].getAttribute("id");
+    var inputElementHour = inputElementId.replace("hourInput", "");
+    if (currentHour > inputElementHour) {
+      $(inputElArray[i]).addClass("past");
+    } else if (currentHour == inputElementHour) {
+      $(inputElArray[i]).addClass("present");
+    } else {
+      $(inputElArray[i]).addClass("future");
+    }
+  }
+};
+
 const loadPage = () => {
   setCurrentDay();
   generateHours();
   populateInfo();
+  relativeTime();
 };
 
 loadPage();
